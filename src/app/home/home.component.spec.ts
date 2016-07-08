08abad628dba7c17a1a4ecdbd7d1d84f4710f7f8
Damian <rascada@users.noreply.github.com>
2016-07-08 14:25:06 +0200

    fix(home-component.spec) remove deprecated beforeEachProviders

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,7 +2,7 @@ import {
   it,
   inject,
   describe,
-  beforeEachProviders,
+  addProviders,
 } from '@angular/core/testing';
 
 // Load the implementations that should be tested
@@ -10,9 +10,9 @@ import { HomeComponent } from './home.component';
 
 describe('Home', () => {
   // provide our implementations or mocks to the dependency injector
-  beforeEachProviders(() => [
-    HomeComponent
-  ]);
+  beforeEach(() => {
+    addProviders([HomeComponent]);
+  });
 
   it('should log ngOnInit', inject([HomeComponent], (home) => {
     spyOn(console, 'log');
